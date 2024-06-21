808d9305455b026d66ac55c3e864e8b3c76f35ea
Shyderth <crisdavidpadilla@gmail.com>
2024-06-20 22:01:51 -0500

    configuracion inicial para el frontend y el backend

--- backend-ktor/src/main/kotlin/com/tienda/plugins/Routing.kt
+++ backend-ktor/src/main/kotlin/com/tienda/plugins/Routing.kt
@@ -1,12 +1,16 @@
 package com.tienda.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
+        singlePageApplication {
+            react("C:\\ProyectosWeb\\proyecto-tienda\\vite-app\\dist")
+        }
+        get("/api") {
             call.respondText("Hello World!")
         }
     }
